75868ce7653cf4014fe35c692aa50f09165ce596
Rybson2115 <szymon.niemyt@gmail.com>
2020-10-12 16:25:55 +0200

    Tests ;)

--- src/main/kotlin/clinicForRSQ/clinic/Visit/VisitService.kt
+++ src/main/kotlin/clinicForRSQ/clinic/Visit/VisitService.kt
@@ -1,29 +1,18 @@
 package clinicForRSQ.clinic.Visit
 
-
-import clinicForRSQ.clinic.Doctor.Doctor
-import clinicForRSQ.clinic.Patient.Patient
 import org.springframework.stereotype.Service
-import java.sql.Time
 import java.time.LocalDate
 import java.time.LocalTime
 
 @Service
 class VisitService (var repo : VisitRepo){
     fun tryAddVisit(visit: Visit){
-        var currentDate = LocalDate.now()
-        var currentTime = LocalTime.now()
-        var opentTime : LocalTime = LocalTime.of(7,59,59) //first visit may start at 8:00
-        var closeTime : LocalTime = LocalTime.of(17,46,0) //last visit may start at 17:45,  clinic works until 18
-        if(checkDateToDay(visit.date))
-            if( opentTime < visit.time && visit.time < closeTime)
-                repo.save(visit)
-        else if(visit.date == currentDate)
-            if( currentTime < visit.time && visit.time < closeTime)
-                repo.save(visit)
+        if(checkDateAndTime(visit.date,visit.time)) {
+            repo.save(visit)
+            Exception("Visit saved!")
+        }
         else
-                Exception("Wrong date or time!")
-
+        Exception("Visit no saved!")
 
     }
     fun tryGetAllVistis() = repo.findAll().toList()
@@ -32,31 +21,27 @@ class VisitService (var repo : VisitRepo){
     fun tryFindDoctorVisits(doctorId: Long) = repo.findDoctortVisitById(doctorId)
     fun tryFindPatientVisits(patientId: Long) = repo.findPatientVisitById(patientId)
     fun tryDeleteVisits(visits : List<Visit>) = repo.deleteAll(visits)
-    fun checkDate(date:LocalDate): Boolean{
-        val currentDate : LocalDate = LocalDate.now()
-        if(date > currentDate)
-            return true
-        return false
-    }
-    fun checkDateToDay(date: LocalDate):Boolean{
-        val currentDate : LocalDate = LocalDate.now()
-        /*if(date == currentDate){
-            return true}
-            return true*/
-        return true
-    }
-    fun checkTime(time: LocalTime): Boolean{
+    fun checkDateAndTime(visitDate: LocalDate, visitTime: LocalTime):Boolean{
+        var closeTime : LocalTime = LocalTime.of(17,46,0) //last visit may start at 17:45,  clinic works until 18
         var opentTime : LocalTime = LocalTime.of(7,59,59) //first visit may start at 8:00
-        var closeTime : LocalTime = LocalTime.of(17,46,0) //last visit may start at 17:45,  clinic works until 18
-        if( opentTime < time && time < closeTime)
-            return true
-        return false
-    }
-    fun checkTimeToDay(time: LocalTime): Boolean{
-        var closeTime : LocalTime = LocalTime.of(17,46,0) //last visit may start at 17:45,  clinic works until 18
-        var currentTime = LocalTime.now()
-        if(currentTime < time && time < closeTime)
-            return true
+        var currentTime: LocalTime = LocalTime.now()
+        val currentDate : LocalDate = LocalDate.now()
+        if(visitDate == currentDate){
+            if(currentTime< visitTime && visitTime < closeTime)
+                return true
+            else{
+                Exception("Wrong time!")
+                return false
+            }}
+        else if(visitDate.isAfter(currentDate)){
+            if(opentTime<visitTime && visitTime < closeTime)
+                return true
+            else{
+                Exception("Wrong time!")
+                return false
+            }}
+        else
+            Exception("Wrong date!")
         return false
     }
 
--- src/test/kotlin/clinicForRSQ/clinic/Visit/VisitServiceTest.kt
+++ src/test/kotlin/clinicForRSQ/clinic/Visit/VisitServiceTest.kt
@@ -1,28 +1,91 @@
 package clinicForRSQ.clinic.Visit
 
-import ch.qos.logback.core.boolex.Matcher
+
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
-import org.junit.runner.RunWith
-import org.mockito.InjectMocks
-import org.mockito.Mock
 import org.mockito.Mockito
-import org.mockito.junit.MockitoJUnitRunner
 import java.time.LocalDate
+import java.time.LocalTime
 
 
 class VisitServiceTest /*(val visitService : VisitService)*/ {
 
     @Test
-    fun checkDateTest() {
+    fun checkDateTimeNowTestTrue() { //current date, correct time, expected result = true
 
         val currentDate : LocalDate = LocalDate.now()
+        val goodTime: LocalTime = LocalTime.of(17,45,0,0)
+
         val result : Boolean = true
-        val date : LocalDate = LocalDate.of(2020,10,11)
-        val visitService: VisitService = Mockito.mock(VisitService::class.java)
-        assertEquals(result, visitService.checkDateToDay(currentDate) )
+        val visitRepo : VisitRepo = Mockito.mock(VisitRepo::class.java)
+        var visitService: VisitService = VisitService(visitRepo)
+        assertEquals(result, visitService.checkDateAndTime(currentDate,goodTime) )
 
     }
-}
+    @Test
+    fun checkDateTimeNowTestFalse() { //current date, incorrect time, expected result = false
 
-//visitService.checkDateToDay(currentDate)
+        val currentDate : LocalDate = LocalDate.now()
+        val goodTime: LocalTime = LocalTime.of(7,0,0,0)
+
+        val result : Boolean = false
+        val visitRepo : VisitRepo = Mockito.mock(VisitRepo::class.java)
+        var visitService: VisitService = VisitService(visitRepo)
+        assertEquals(result, visitService.checkDateAndTime(currentDate,goodTime) )
+
+    }
+    @Test
+    fun checkDateTimeTestTrue() { //correct date, correct time, expected result = true
+
+        val currentDate : LocalDate = LocalDate.now()
+        val goodDate: LocalDate = LocalDate.of(currentDate.year+1,1,1)
+        val goodTime: LocalTime = LocalTime.of(12,0,0,0)
+
+        val result : Boolean = true
+        val visitRepo : VisitRepo = Mockito.mock(VisitRepo::class.java)
+        var visitService: VisitService = VisitService(visitRepo)
+        assertEquals(result, visitService.checkDateAndTime(goodDate,goodTime) )
+
+    }
+    @Test
+    fun checkDateTimeTestFalseTime() { //correct date, incorrect time, expected result = true
+
+        val currentDate : LocalDate = LocalDate.now()
+        val goodDate: LocalDate = LocalDate.of(currentDate.year+1,1,1)
+        val goodTime: LocalTime = LocalTime.of(6,0,0,0)
+
+        val result : Boolean = false
+        val visitRepo : VisitRepo = Mockito.mock(VisitRepo::class.java)
+        var visitService: VisitService = VisitService(visitRepo)
+        assertEquals(result, visitService.checkDateAndTime(goodDate,goodTime) )
+
+    }
+    @Test
+    fun checkDateTimeTestFalseDate() { //incorrect date, correct time, expected result = true
+
+        val currentDate : LocalDate = LocalDate.now()
+        val goodDate: LocalDate = LocalDate.of(currentDate.year-1,1,1)
+        val goodTime: LocalTime = LocalTime.of(12,0,0,0)
+
+        val result : Boolean = false
+        val visitRepo : VisitRepo = Mockito.mock(VisitRepo::class.java)
+        var visitService: VisitService = VisitService(visitRepo)
+        assertEquals(result, visitService.checkDateAndTime(goodDate,goodTime) )
+
+    }
+    @Test
+    fun checkDateTimeTestFalseDateTime() { //incorrect date, incorrect time, expected result = true
+
+        val currentDate : LocalDate = LocalDate.now()
+        val goodDate: LocalDate = LocalDate.of(currentDate.year-1,1,1)
+        val goodTime: LocalTime = LocalTime.of(7,0,0,0)
+
+        val result : Boolean = false
+        val visitRepo : VisitRepo = Mockito.mock(VisitRepo::class.java)
+        var visitService: VisitService = VisitService(visitRepo)
+        assertEquals(result, visitService.checkDateAndTime(goodDate,goodTime) )
+
+    }
+
+
+}
